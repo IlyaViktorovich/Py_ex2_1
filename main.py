ab99a71f3a7a1da4e25eeae835a53fada3c47957
IlyaViktorovich <giv123@mail.ru>
2021-12-17 07:16:56 +0300

    второй

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-def read_txt(file_name, encoding="utf-8"):
-    cook_recipe = []
-    with open(file_name) as file:
-        #print(type(file))
+def read_txt(file_name):
+
+    cook_book = {}
+    with open(file_name, encoding="utf-8") as file:
+        cook_recipe = []
+        #print(type(file)) cook_recipe = []
         for line in file:
-            cook_recipe.append(line.strip())
-            number_recipe = int(file.readline().strip())
-            for line_ingredients in range(number_recipe):
-                print(file.readline())
+            cook_recipe.append(line)
+            number_recipe = int(file.readline())
+            for numer_ingredients in range(number_recipe):
+                res = file.readline().strip().strit(' | ')
+                print(res)
+                #print(type(file.readline()))
+                #print(line_ingredients)
             file.readline()
 
     #print(cook_recipe)
+    #return cook_book
 
 read_txt('recipes.txt')

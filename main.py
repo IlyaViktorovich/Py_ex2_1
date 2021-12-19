681a7928e19fd4592478493351bb644b66ab5c71
IlyaViktorovich <giv123@mail.ru>
2021-12-20 01:12:54 +0300

    ghjkt;

--- main.py
+++ main.py
@@ -1,20 +1,48 @@
 def read_txt(file_name):
-
     cook_book = {}
+    cook_list = []
     with open(file_name, encoding="utf-8") as file:
-        cook_recipe = []
-        #print(type(file)) cook_recipe = []
         for line in file:
-            cook_recipe.append(line)
-            number_recipe = int(file.readline())
+            cook_name = line.strip()
+            cook_list.append(cook_name)
+            number_recipe = int(file.readline().strip())
+            ingrid = []
             for numer_ingredients in range(number_recipe):
-                res = file.readline().strip().strit(' | ')
-                print(res)
-                #print(type(file.readline()))
-                #print(line_ingredients)
+                res = file.readline().strip().split(' | ')
+                dich = {}
+                dich['ingredient_name'] = res[0]
+                dich['quantity'] = res[1]
+                dich['measure'] = res[2]
+                ingrid.append(dich)
+            cook_book[cook_name] = ingrid
             file.readline()
+    return cook_book
 
-    #print(cook_recipe)
-    #return cook_book
+def get_shop_list_by_dishes(dishes, person_count):
+  dict = {}
+  cook_book = read_txt('recipes.txt')
+  for key in cook_book:
+    for name_cook in dishes:
+      if key == name_cook:
+        for key2 in cook_book[key]:
+          doct = {}
+          for key3 in key2:
+            if key3 == 'ingredient_name':
+              name_ing = key2[key3]
+            elif key3 == 'measure':
+              meas = key2[key3]
+              doct["measure"] = meas
+              doct['quantity'] = quant
+            elif key3 == 'quantity':
+              quant = key2[key3]*person_count
+          dict[name_ing] = doct
+  return dict
 
-read_txt('recipes.txt')
+
+
+
+
+#result = read_txt('recipes.txt')
+
+
+print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2 ))
